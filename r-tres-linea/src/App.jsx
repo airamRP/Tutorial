3e9da25742fc9b7da34c49d06f09b196fbb37407
airamRP <rpairam18@gmail.com>
2024-10-16 13:39:08 +0200

    Día 2. Pensar en React

--- r-tres-linea/src/App.jsx
+++ r-tres-linea/src/App.jsx
@@ -1,14 +1,22 @@
-import {Game} from './Index.jsx'
+import {FilterableProductTable} from './Index.jsx'
 import './App.css'
 
 function App() {
 
+  const PRODUCTS = [
+    { category: "Frutas", price: "$1", stocked: true, name: "Manzana" },
+    { category: "Frutas", price: "$1", stocked: true, name: "Fruta del dragón" },
+    { category: "Frutas", price: "$2", stocked: false, name: "Maracuyá" },
+    { category: "Verduras", price: "$2", stocked: true, name: "Espinaca" },
+    { category: "Verduras", price: "$4", stocked: false, name: "Calabaza" },
+    { category: "Verduras", price: "$1", stocked: true, name: "Guisantes" }
+  ];
+  
+
   return (
     <>
-
-      <h1>Tres en línea</h1>
       <div className="card">
-        <Game />
+        <FilterableProductTable products={PRODUCTS}/>
       </div>
 
     </>
